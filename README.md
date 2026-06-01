d624887f620799e3f754c134a61e0bcdf931437f
Patrícia Silva <164388986+Silv-Patricia@users.noreply.github.com>
2026-05-31 22:26:47 -0300

    correção de bugs

--- README.md
+++
@@ -1,3 +0,0 @@
-# marimba_estrutura_inicial_flutter
-
-A new Flutter project.
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(MarimbaApp());
 
@@ -8,9 +9,49 @@ class MarimbaApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              teclaMarimba(Color(0xFF009788), 1),
+              teclaMarimba(Color(0xFF2296F3), 2),
+              teclaMarimba(Color(0xFFFFEB3B), 3),
+              teclaMarimba(Color(0xFFF34436), 4),
+              teclaMarimba(Color(0xFF9E9E9E), 5),
+              teclaMarimba(Color(0xFF9C27B0), 6),
+              teclaMarimba(Color(0xFF795548), 7),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+Widget teclaMarimba(Color corBotao, int numeroNota) {
+  final player = AudioPlayer();
+
+  player.setAudioContext(
+    AudioContext(
+      android: AudioContextAndroid(
+        audioFocus:
+            AndroidAudioFocus.none, // Diz ao Android para não disputar o foco
+      ),
+      iOS: AudioContextIOS(
+        options: {
+          AVAudioSessionOptions
+              .mixWithOthers, // Garante que também funciona bem em iPhones
+        },
+      ),
+    ),
+  );
+
+  return Expanded(
+    child: GestureDetector(
+      onTap: () async {
+        
+        await player.stop();
+        await player.play(AssetSource('sounds/nota$numeroNota.wav'));
+      },
+      child: Container(color: corBotao),
+    ),
+  );
 }
